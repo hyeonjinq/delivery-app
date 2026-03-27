cfaf26804df9eab5af43ccb655acbba895b88c17
222472 <rion@plgrim.com>
2026-03-27 10:53:08 +0900

    first commit

---
+++ src/main/kotlin/com/fastcampus/delivery_app/DeliveryAppApplication.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.delivery_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeliveryAppApplication
+
+fun main(args: Array<String>) {
+	runApplication<DeliveryAppApplication>(*args)
+}
